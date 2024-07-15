b6cbc463449881b274d0881490304417c9f9c5fb
inpyo <winsowss@hanmail.net>
2024-07-15 16:15:41 +0900

    [feat] read/fullread 초기구현

--- testapp/command/fullread.py
+++ testapp/command/fullread.py
@@ -1,6 +1,20 @@
+from testapp.command import Read
 from testapp.command.__interface import CommandInterface
+
+STARTADDR = 0
+ENDADDR = 99
 
 
 class FullRead(CommandInterface):
+    def __init__(self):
+        self.read = Read()
+
     def run(self, *args, **kwarg):
-        pass
+        self.valid_check(args)
+
+        for addr in range(STARTADDR, ENDADDR + 1):
+            print(self.read.run(addr))
+
+    def valid_check(self, args):
+        if len(args) != 0:
+            raise ValueError
--- testapp/command/read.py
+++ testapp/command/read.py
@@ -1,8 +1,22 @@
 from testapp.command.__interface import CommandInterface
+from tests.util import get_ssd_result
 
 
 class Read(CommandInterface):
     def run(self, *args, **kwarg):
-        return '0x12345678'
+        self.valid_check(args)
+
+        read_cmd = f'ssd.py R {args[0]}'
+        run_subprocess(read_cmd)
+
+        return get_ssd_result()
+
+    def valid_check(self, args):
+        if len(args) != 1:
+            raise ValueError
+        if args[0] < 0:
+            raise ValueError
+        elif args[0] >= 100:
+            raise ValueError
 
 
