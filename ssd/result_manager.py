61ebb40c60421a769b3a0bf7a9f236562ce890aa
jwh1308 <67500007+jwh1308@users.noreply.github.com>
2024-07-15 16:11:58 +0900

    [refactoring] result.txt 파일 생성 및 초기화 코드 추가

--- ssd/result_manager.py
+++ ssd/result_manager.py
@@ -1,7 +1,12 @@
+from pathlib import Path
+
+RESULT_FILE_PATH = Path(__file__).parent / 'result.txt'
+
+
 class ResultManager:
 
     def __init__(self):
-        self.result_file_path = None
+        self.result_file_path = RESULT_FILE_PATH
 
     def write(self, text):
         with open(self.result_file_path, 'w') as f:
