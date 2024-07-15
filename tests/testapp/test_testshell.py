fe5fab0cb1487494ee17476e4f754f6eb1c4a0a6
bj93.park <qazws9346@gmail.com>
2024-07-15 16:06:12 +0900

    [FEAT] TCs for convert_to_vald_cmd and valid_cmd

--- tests/testapp/test_testshell.py
+++ tests/testapp/test_testshell.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from unittest.mock import patch
 
 from testapp.testshell import TestShell
 
@@ -7,6 +8,122 @@ class TestTestShell(TestCase):
     def setUp(self):
         super().setUp()
         self.testshell = TestShell()
+
+    def test_valid_cmd_true(self):
+        cmd_list = [
+            "write 3 0xAAAABBBB",
+            "read 3",
+            "exit",
+            "help",
+            "fullwrite 0xABCDFFFF",
+            "fullread",
+            "testapp1",
+            "testapp2",
+        ]
+        for cmd in cmd_list:
+            ret = self.testshell.valid_cmd(cmd)
+            self.assertTrue(ret)
+
+    def test_valid_cmd_write_false(self):
+
+        cmd_list = [
+            "Write 3 0xAAAABBBB",
+            "Write 3 0xAAAABBBB",
+            "write 0xAAAABBBB",
+            "write 3",
+            "write",
+        ]
+        for cmd in cmd_list:
+            ret = self.testshell.valid_cmd(cmd)
+            self.assertFalse(ret)
+
+    def test_valid_cmd_read_false(self):
+
+        cmd_list = [
+            "read 3 0xAAAABBBB",
+            "Read 3",
+            "read 0xAAAABBBB",
+        ]
+        for cmd in cmd_list:
+            ret = self.testshell.valid_cmd(cmd)
+            self.assertFalse(ret)
+
+    def test_valid_cmd_exit_false(self):
+
+        cmd_list = [
+            "Exit",
+            "exit 1",
+            "exit 0xABCDFFFF",
+        ]
+        for cmd in cmd_list:
+            ret = self.testshell.valid_cmd(cmd)
+            self.assertFalse(ret)
+
+    def test_valid_cmd_help_false(self):
+
+        cmd_list = [
+            "HELP",
+            "hELP",
+            "help 0xABCDFFFF",
+        ]
+        for cmd in cmd_list:
+            ret = self.testshell.valid_cmd(cmd)
+            self.assertFalse(ret)
+
+    def test_valid_cmd_fullwrite_false(self):
+
+        cmd_list = [
+            "Fullwrite 0xABCDFFFF",
+            "FULLWRITE 0xABCDFFFF",
+            "FullWrite 0xABCDFFFF",
+            "fullwrite ABCDFFFF",
+        ]
+        for cmd in cmd_list:
+            ret = self.testshell.valid_cmd(cmd)
+            self.assertFalse(ret)
+
+    def test_valid_cmd_fullread_false(self):
+
+        cmd_list = [
+            "Fullread",
+            "FULLREAD",
+            "FullRead",
+        ]
+        for cmd in cmd_list:
+            ret = self.testshell.valid_cmd(cmd)
+            self.assertFalse(ret)
+
+    @patch.object(TestShell, 'valid_cmd', return_value=True)
+    def test_convert_to_valid_cmd_true(self):
+        cmd_list = [
+            "write 3 0xAAAABBBB",
+            "read 3",
+            "exit",
+            "help",
+            "fullwrite 0xABCDFFFF",
+            "fullread",
+            "testapp1",
+            "testapp2",
+        ]
+        for cmd in cmd_list:
+            ret = self.testshell.convert_to_valid_cmd(cmd)
+            self.assertEqual(ret, cmd)
+
+    @patch.object(TestShell, 'valid_cmd', return_value=False)
+    def test_convert_to_valid_cmd_false(self):
+        cmd_list = [
+            "write 3 0xAAAABBBB",
+            "read 3",
+            "exit",
+            "help",
+            "fullwrite 0xABCDFFFF",
+            "fullread",
+            "testapp1",
+            "testapp2",
+        ]
+        for cmd in cmd_list:
+            ret = self.testshell.convert_to_valid_cmd(cmd)
+            self.assertEqual(ret, "INVALID COMMAND")
 
     def test_testshell_write(self):
         cmd = "write 3 0xAAAABBBB"
