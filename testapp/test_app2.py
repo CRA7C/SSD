2cfc2df2bb784a84d6520e7b03194d3d1a508ff6
jong2.ha <gk1whd2@gmail.com>
2024-07-16 09:45:34 +0900

    [FEAT] TestApp2 추가.

--- testapp/test_app2.py
+++ testapp/test_app2.py
@@ -4,3 +4,9 @@ TestApp2 제작하기
  • 0 ~ 5 번 LBA 에 0x12345678 값으로 1 회 Over Write를 수행한다.
  • 0 ~ 5 번 LBA Read 했을 때 정상적으로 값이 읽히는지 확인한다
 """
+from testapp.command.__interface import CommandInterface
+
+
+class TestApp2(CommandInterface):
+    def run(self, *args, **kwargs):
+        pass
---
+++ tests/testapp/test_test_app2.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+from testapp.test_app2 import TestApp2
+
+
+class TestTestApp2(TestCase):
+    def test_run(self):
+        pass
