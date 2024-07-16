4a7c6f5ab7b3de2b2d450dd40c9c09d2c214b258
jong2.ha <gk1whd2@gmail.com>
2024-07-16 09:58:27 +0900

    [FEAT] TestApp2: write_30_times 추가

--- testapp/test_app2.py
+++ testapp/test_app2.py
@@ -4,9 +4,22 @@ TestApp2 제작하기
  • 0 ~ 5 번 LBA 에 0x12345678 값으로 1 회 Over Write를 수행한다.
  • 0 ~ 5 번 LBA Read 했을 때 정상적으로 값이 읽히는지 확인한다
 """
+from testapp.command import Write, Read
 from testapp.command.__interface import CommandInterface
+
+WRITE_VALUE = 0xAAAABBBB
 
 
 class TestApp2(CommandInterface):
     def run(self, *args, **kwargs):
-        pass
+        self.write_30_times()
+
+    def write_30_times(self):
+        write = Write()
+        try:
+            for _ in range(6):
+                for lba in range(5):
+                    write.run(lba, WRITE_VALUE)
+        except Exception:
+            raise Exception
+        return True
--- tests/testapp/test_test_app2.py
+++ tests/testapp/test_test_app2.py
@@ -1,8 +1,22 @@
 from unittest import TestCase
+from unittest.mock import patch
 
+from testapp.command import Read, Write
 from testapp.test_app2 import TestApp2
 
 
 class TestTestApp2(TestCase):
-    def test_run(self):
-        pass
+
+    @patch.object(Write, 'run', return_value=True)
+    def test_write_30_times(self, write_mock):
+        self.test_app2 = TestApp2()
+        self.assertTrue(self.test_app2.write_30_times())
+
+    @patch.object(Write, 'run', return_value=True)
+    def test_write_30_times_SHOULD_execute_write_30_times(self, write_mock):
+        self.test_app2 = TestApp2()
+        self.test_app2.write_30_times()
+
+        self.assertEqual(30, write_mock.call_count)
+
+
