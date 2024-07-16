3675a297bc2740f1240083f83fb3f77cdde2d7d9
geniuskey <geniuskey@gmail.com>
2024-07-16 09:18:36 +0900

    ssd_driver 구현 및 테스트 구현 완료

--- tests/testapp/test_ssd_driver.py
+++ tests/testapp/test_ssd_driver.py
@@ -16,33 +16,34 @@ class TestSsdDriver(unittest.TestCase):
         mock_run.return_value.stdout = "Command output"
         mock_run.return_value.stderr = ""
 
-        command = "ls"
-        stdout, stderr = self.driver._run("ssd R 2")
+        command = "python ssd R 2"
+        result = self.driver.run_subprocess(command.replace("python ", ""))
 
         # subprocess.run이 올바른 인자와 함께 호출되었는지 확인
         mock_run.assert_called_once_with(
             command,
             shell=True,
-            cwd=Path(__file__).parent.parent,
+            check=True,
+            cwd=Path(__file__).parent.parent.parent,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True
         )
 
-        self.assertEqual(stdout, "Command output")
-        self.assertEqual(stderr, "")
+        self.assertEqual(result.stdout, "Command output")
+        self.assertEqual(result.stderr, "")
 
-    @patch('testapp.ssd_driver.SsdDriver._run')
+    @patch('testapp.ssd_driver.SsdDriver.run_subprocess')
     def test_read(self, mock_run):
         test_lba = 3
         expected_command = f"ssd R {test_lba}"
         self.driver.read(test_lba)
-        mock_run._run.assert_called_once_with(expected_command)
+        mock_run.assert_called_once_with(expected_command)
 
-    @patch('testapp.ssd_driver.SsdDriver._run')
+    @patch('testapp.ssd_driver.SsdDriver.run_subprocess')
     def test_write(self, mock_run):
         test_lba = 3
         test_value = 0x1289CDEF
-        expected_command = f"ssd R {test_lba} 0x{test_value:08X}"
+        expected_command = f"ssd W {test_lba} 0x{test_value:08X}"
         self.driver.write(test_lba, test_value)
-        mock_run._run.assert_called_once_with(expected_command)
+        mock_run.assert_called_once_with(expected_command)
