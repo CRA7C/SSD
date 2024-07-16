8455afb04e37193f1834e6b80384960b1e2e8439
bj93.park <qazws9346@gmail.com>
2024-07-16 11:58:46 +0900

    [FIX] add INVALID COMMAND

--- testapp/testshell.py
+++ testapp/testshell.py
@@ -47,18 +47,18 @@ class TestShell:
 
     def execute(self, cmd: str) -> int:
         if not self.is_valid_cmd(cmd):
-            print(cmd)
+            print(INVALID_COMMAND)
             return EXECUTE_INVALID
 
         cmd_option, cmd_args = self.parse_args(cmd)
 
         cmd_if = self.cmd_if_dict[cmd_option]()
         if cmd_args is not None:
-            print(cmd_option, cmd_args)
+            print(cmd_option, *cmd_args)
             cmd_if.run(*cmd_args)
             return EXECUTE_VALID_WITH_ARGS
 
-        print(cmd_option, cmd_args)
+        print(cmd_option)
         cmd_if.run()
         return EXECUTE_VALID_WO_ARGS
 
