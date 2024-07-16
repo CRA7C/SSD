642a6fb5516872d4f30bcaab2d8fb750ecc3861e
geniuskey <geniuskey@gmail.com>
2024-07-16 09:41:44 +0900

    command.Write & FullWrite 구현 및 테스트 구현 완료

--- testapp/command/fullwrite.py
+++ testapp/command/fullwrite.py
@@ -1,6 +1,13 @@
 from testapp.command.__interface import CommandInterface
+from testapp.ssd_driver import SsdDriver
+from testapp.constants import SSD_SIZE
 
 
 class FullWrite(CommandInterface):
-    def run(self, *args, **kwarg):
-        pass
+    def __init__(self):
+        super().__init__()
+        self.driver = SsdDriver()
+
+    def run(self, value):
+        for i in range(SSD_SIZE):
+            self.driver.write(i, value)
