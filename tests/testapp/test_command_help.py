f5c3711b9b1c8e425cd97276b65b304434583895
geniuskey <geniuskey@gmail.com>
2024-07-16 09:48:21 +0900

    command.Help 구현 및 테스트 구현 완료

---
+++ tests/testapp/test_command_help.py
@@ -0,0 +1,20 @@
+import sys
+import io
+import unittest
+from testapp.command import Help
+
+
+class TestHelp(unittest.TestCase):
+    def test_run(self):
+        command = Help()
+        sys_stdout = sys.stdout  # 원래의 sys.stdout을 저장
+        sys.stdout = io.StringIO()
+        command.run()
+        help_output = sys.stdout.getvalue()
+        sys.stdout = sys_stdout
+        for key in ['write', 'read', 'exit', 'help', 'fullwrite', 'fullread']:
+            self.assertTrue(key in help_output)
+
+
+if __name__ == '__main__':
+    unittest.main()
