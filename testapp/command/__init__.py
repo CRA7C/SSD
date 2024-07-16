1f1af09b081966ad994d887d5bae4a11fb06cb75
geniuskey <geniuskey@gmail.com>
2024-07-16 10:56:48 +0900

    tests 폴더에 대한 unittest 시 import 오류 발생으로 해당 폴더 이름 변경

--- testapp/command/__init__.py
+++ testapp/command/__init__.py
@@ -5,4 +5,15 @@ from .help import Help
 from .fullwrite import FullWrite
 from .fullread import FullRead
 
-__all__ = ['Write', 'Read', 'Exit', 'Help', 'FullWrite', 'FullRead']
+COMMAND_DICT = {
+    "write": Write,
+    "read": Read,
+    "exit": Exit,
+    "help": Help,
+    "fullwrite": FullWrite,
+    "fullread": FullRead,
+    # "testapp1": command.Testapp1,
+    # "testapp2": command.Testapp2,
+}
+__all__ = ['Write', 'Read', 'Exit', 'Help', 'FullWrite', 'FullRead',
+           'COMMAND_DICT']
