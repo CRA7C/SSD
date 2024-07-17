a38bbb362a4133caf2af6a9df7d847715c963e33
jwh1308 <67500007+jwh1308@users.noreply.github.com>
2024-07-17 11:47:21 +0900

    [feature] 삭제 가능한 사이즈 넘어설 경우, 마지막 LBA까지 삭제하도록 자동 명령어 수정 기능 추가

--- testapp/command/erase.py
+++ testapp/command/erase.py
@@ -10,9 +10,10 @@ class Erase(CommandInterface):
 
     def run(self, lba, total_size):
         erase_size = int(total_size)
-        remain_erase_size = erase_size
         initial_lba = int(lba)
-        for start_lba in range(initial_lba, initial_lba + erase_size, MAXIMUM_ERASE_SIZE_AT_ONCE):
+        end_lba = 100 if initial_lba + erase_size > 100 else initial_lba + erase_size
+        remain_erase_size = end_lba - initial_lba
+        for start_lba in range(initial_lba, end_lba, MAXIMUM_ERASE_SIZE_AT_ONCE):
             if remain_erase_size > MAXIMUM_ERASE_SIZE_AT_ONCE:
                 size = MAXIMUM_ERASE_SIZE_AT_ONCE
                 remain_erase_size -= size
--- tests/testapp_test/test_command_erase.py
+++ tests/testapp_test/test_command_erase.py
@@ -19,3 +19,9 @@ class TestErase(TestCase):
         self.cmd.run('0', '33')
         self.assertEqual(4, mock_erase.call_count)
         mock_erase.assert_called_with(30, 3)
+
+    @patch.object(SsdDriver, 'erase')
+    def test_run_when_erase_size_greater_than_100(self, mock_erase):
+        self.cmd.run('85', '110')
+        self.assertEqual(2, mock_erase.call_count)
+        mock_erase.assert_called_with(95, 5)
