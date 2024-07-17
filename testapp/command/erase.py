c9b57e65d15a47a396640c693a82ec9bc18a13e4
jwh1308 <67500007+jwh1308@users.noreply.github.com>
2024-07-17 11:35:29 +0900

    [feature] 전달받은 erase 사이즈가 10보다 클 경우 처리 로직 추가

--- testapp/command/erase.py
+++ testapp/command/erase.py
@@ -6,5 +6,15 @@ class Erase(CommandInterface):
     def __init__(self):
         self.driver = SsdDriver()
 
-    def run(self, lba, size):
-        self.driver.erase(lba, int(size))
+    def run(self, lba, total_size):
+        erase_size = int(total_size)
+        remain_erase_size = erase_size
+        initial_lba = int(lba)
+        for start_lba in range(initial_lba, initial_lba + erase_size, 10):
+            if remain_erase_size > 10:
+                size = 10
+                remain_erase_size -= size
+            else:
+                size = remain_erase_size
+                remain_erase_size = 0
+            self.driver.erase(start_lba, size)
--- tests/testapp_test/test_command_erase.py
+++ tests/testapp_test/test_command_erase.py
@@ -10,4 +10,11 @@ class TestErase(TestCase):
     def test_run(self, mock_erase):
         self.cmd = Erase()
         self.cmd.run('0', '5')
-        mock_erase.assert_called_with('0', 5)
+        mock_erase.assert_called_with(0, 5)
+
+    @patch.object(SsdDriver, 'erase')
+    def test_run_when_erase_size_greater_than_10(self, mock_erase):
+        self.cmd = Erase()
+        self.cmd.run('0', '33')
+        self.assertEqual(4, mock_erase.call_count)
+        mock_erase.assert_called_with(30, 3)
