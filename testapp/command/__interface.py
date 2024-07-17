771b3b68ded8cbcbf702727da9874f2ecc2c116e
jwh1308 <67500007+jwh1308@users.noreply.github.com>
2024-07-17 16:20:20 +0900

    [refactor] interface 변경

--- testapp/command/__interface.py
+++ testapp/command/__interface.py
@@ -5,3 +5,8 @@ class CommandInterface(ABC):
     @abstractmethod
     def run(self, *args, **kwarg):
         pass
+
+    @staticmethod
+    @abstractmethod
+    def is_valid_args(self, *args, **kwarg):
+        pass
