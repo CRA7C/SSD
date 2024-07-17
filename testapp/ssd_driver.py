9f7b1c4da8388f928ab1d55db72cbce44581ee88
geniuskey <geniuskey@gmail.com>
2024-07-17 13:52:55 +0900

    `SsdDriver.write` 의 동작을 좀 더 유연하게 변경.

--- testapp/ssd_driver.py
+++ testapp/ssd_driver.py
@@ -29,5 +29,7 @@ class SsdDriver:
         self.run_subprocess(f"ssd R {lba}")
         return get_ssd_result()
 
-    def write(self, lba: str | int, value: str):
+    def write(self, lba: str | int, value: str | int):
+        if isinstance(value, int):
+            value = f"0x{value:08X}"
         self.run_subprocess(f"ssd W {lba} {value}")
