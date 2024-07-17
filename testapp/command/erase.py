e82fcccfe166f83b468ab0379618f4eaedb5b5c3
jwh1308 <67500007+jwh1308@users.noreply.github.com>
2024-07-17 11:38:21 +0900

    [refactor] 중복 코드 & 매직 넘버 제거

--- testapp/command/erase.py
+++ testapp/command/erase.py
@@ -1,6 +1,8 @@
 from testapp.command.__interface import CommandInterface
 from testapp.ssd_driver import SsdDriver
 
+MAXIMUM_ERASE_SIZE_AT_ONCE = 10
+
 
 class Erase(CommandInterface):
     def __init__(self):
@@ -10,9 +12,9 @@ class Erase(CommandInterface):
         erase_size = int(total_size)
         remain_erase_size = erase_size
         initial_lba = int(lba)
-        for start_lba in range(initial_lba, initial_lba + erase_size, 10):
-            if remain_erase_size > 10:
-                size = 10
+        for start_lba in range(initial_lba, initial_lba + erase_size, MAXIMUM_ERASE_SIZE_AT_ONCE):
+            if remain_erase_size > MAXIMUM_ERASE_SIZE_AT_ONCE:
+                size = MAXIMUM_ERASE_SIZE_AT_ONCE
                 remain_erase_size -= size
             else:
                 size = remain_erase_size
