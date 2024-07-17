3e7217a152bb09ea9c2f8305fc0e24d3cd64ffe7
bj93.park <qazws9346@gmail.com>
2024-07-17 11:14:01 +0900

    [FEAT] erase in ssd

--- ssd/__main__.py
+++ ssd/__main__.py
@@ -14,10 +14,13 @@ class SSDRunner:
     @staticmethod
     def is_valid_command():
         if len(sys.argv) < 3:
-            raise ValueError("명령을 수행하기 위한 인자가 부족합니다. ex) ssd R 20/ssd W 20 0x1289CDEF")
+            raise ValueError("명령을 수행하기 위한 인자가 부족합니다. ex) ssd R 20/ssd W 20 0x1289CDEF/ssd E 10 3")
 
-        if sys.argv[1] not in ('R', 'W'):
-            raise ValueError('R 또는 W를 사용해주세요.(대문자)')
+        if sys.argv[1] not in ('R', 'W', 'E'):
+            raise ValueError('R, W, E 중 하나를 사용해주세요.(대문자)')
+
+        if not sys.argv[2].isdigit():
+            raise ValueError('LBA는 숫자여야합니다.')
 
         if not LBA_LOWER_LIMIT <= int(sys.argv[2]) <= LBA_UPPER_LIMIT:
             raise ValueError(f'LBA는 {LBA_LOWER_LIMIT} ~ {LBA_UPPER_LIMIT} 여야합니다.')
@@ -27,7 +30,13 @@ class SSDRunner:
                 raise ValueError('W 명령에는 value가 필요합니다.')
             if not is_valid_hex(sys.argv[3]):
                 raise ValueError('value는 0x00000000 형식이여야 합니다.')
-            int(sys.argv[3], 16)
+        if sys.argv[1] == 'E':
+            if len(sys.argv) < 4:
+                raise ValueError('E 명령에는 시작 LBA, 지울 데이터 갯수가 필요합니다.')
+            if not sys.argv[3].isdigit():
+                raise ValueError('지울 데이터 갯수는 숫자여야합니다.')
+            if not 1 <= int(sys.argv[3]) <= 10:
+                raise ValueError('지울 데이터 갯수는 1이상 10이하만 가능합니다.')
         return True
 
     def run(self):
@@ -37,6 +46,9 @@ class SSDRunner:
         elif sys.argv[1] == 'W':
             value = int(sys.argv[3], 16)
             self.ssd.write(lba, value)
+        elif sys.argv[1] == 'E':
+            n_value = int(sys.argv[3])
+            self.ssd.erase(lba, n_value)
 
 
 if __name__ == '__main__':
