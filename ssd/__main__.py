dccf75ee9fdf8ba01bd6deeb4d0d62714197f49a
johnny <gk1whd2@gmail.com>
2024-07-18 15:14:16 +0900

    [Logging] SSDRunner Logging 추가.

--- ssd/__main__.py
+++ ssd/__main__.py
@@ -7,6 +7,9 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from ssd.solidstatedrive import SolidStateDrive
 from ssd.command_buffer import CommandBuffer
 from ssd.command import CommandFactory, Command, ReadCommand, WriteCommand, EraseCommand
+from my_logger import Logger
+
+logger = Logger()
 
 
 class SSDRunner:
@@ -26,6 +29,7 @@ class SSDRunner:
         """
         명령 버퍼를 비우고 명령어를 실행합니다.
         """
+        logger.debug("Buffer Flash")
         cmd_list = self.option_buf.flush()
         for cmd in cmd_list:
             self.execute_command(cmd)
@@ -38,13 +42,17 @@ class SSDRunner:
             self.buff_flush()
         elif cmd.option == 'R':
             if self.option_buf.is_able_to_fast_read(cmd):
-                self.ssd.read_fast(self.option_buf.get_read_fast(cmd))
+                read_value = self.option_buf.get_read_fast(cmd)
+                self.ssd.read_fast(read_value)
+                logger.debug(f"Read Fast : {read_value}")
             else:
                 self.execute_command(cmd)
+                logger.debug("Read from NAND")
         elif cmd.option in ('W', 'E'):
             if self.option_buf.need_flush():
                 self.buff_flush()
             self.option_buf.push_command(cmd)
+            logger.debug("Push into buffer")
 
     def execute_command(self, command: Union[ReadCommand, WriteCommand, EraseCommand]):
         """
@@ -53,6 +61,7 @@ class SSDRunner:
         Args:
             command: 실행할 명령어 객체
         """
+        logger.debug(f"Execute CMD : {command.get_value()}")
         cmd_opt = command.option
         if cmd_opt == 'R':
             self.ssd.read(command.lba)
@@ -63,6 +72,9 @@ class SSDRunner:
 
 
 if __name__ == '__main__':
+    logger.debug("[SSD] Start SSDRunner")
     runner = SSDRunner()
     cmd_ = CommandFactory.parse_command(sys.argv[1:])
+    logger.debug(f"[SSD] Received CMD : {cmd_.get_value()}")
     runner.run(cmd_)
+    logger.debug("[SSD] Finish SSDRunner")
